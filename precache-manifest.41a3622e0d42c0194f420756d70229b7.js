60e1af4b488a5d1c57f6f959bac3bea8a50c2dbc
Kien Quan <hotrankienquan144@gmail.com>
2022-01-15 15:21:23 +0700

    Updates

---
+++ precache-manifest.41a3622e0d42c0194f420756d70229b7.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a1b213122566d9043ba559a127b28b75",
+    "url": "/our-menu-reactjs-fcc/index.html"
+  },
+  {
+    "revision": "7afcca44d98d4117aa52",
+    "url": "/our-menu-reactjs-fcc/static/css/main.26e112f5.chunk.css"
+  },
+  {
+    "revision": "6e331cfd5d1a1283573a",
+    "url": "/our-menu-reactjs-fcc/static/js/2.66b87b03.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/our-menu-reactjs-fcc/static/js/2.66b87b03.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7afcca44d98d4117aa52",
+    "url": "/our-menu-reactjs-fcc/static/js/main.9990510e.chunk.js"
+  },
+  {
+    "revision": "0e2a94f3b2b076aa62d6",
+    "url": "/our-menu-reactjs-fcc/static/js/runtime-main.edf26739.js"
+  }
+]);
